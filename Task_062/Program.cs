6d4350c4382b514474c21b3bde772c45e74a9695
Kulikov Alexander <shouter84@yandex.ru>
2023-06-07 23:57:36 +0300

    Added solution for task 062

--- Task_062/Program.cs
+++ Task_062/Program.cs
@@ -12,8 +12,10 @@ void Main()
 {
     int row = ReadInt("Enter number of rows in genarating array: ");
     int column = ReadInt("Enter number of columns in genarating array: ");
-    int[,] matrix = FillMatrix(row, column, 0, 10);
+    int[,] matrix = FillMatrix(row, column, 0);
     PrintMatrix(matrix);
+    System.Console.WriteLine();
+    PrintMatrix(ArrayNumbering(matrix, row, column, 0));
 
 }
 
@@ -23,19 +25,73 @@ int ReadInt(string text)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-int[,] FillMatrix(int row, int col, int leftRange)
+int[,] FillMatrix(int row, int col, int n)
 {
     int[,] matrix = new int[row, col];
-    int number = leftRange;
+
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] += number;
+            matrix[i, j] = n;
         }
     }
 
     return matrix;
+}
+
+int[,] ArrayNumbering(int[,] arr, int row, int column, int N)
+{
+    int i = 0;
+    int j = 0;
+    while (j <= column - 1)
+    {
+        arr[i, j] = N + 1; N++; j++;
+    }
+
+    i = 1;
+    j = column - 1;
+    while (i <= row - 1)
+    {
+        arr[i, j] = N + 1; N++; i++;
+    }
+
+    i = row - 1;
+    j = column - 2;
+    while (j >= 0)
+    {
+        arr[i, j] = N + 1; N++; j--;
+    }
+
+    i = row - 2;
+    j = 0;
+    while (i >= 1)
+    {
+        arr[i, j] = N + 1; N++; i--;
+    }
+
+    i = 1;
+    j = 1;
+    while (j <= column - 2)
+    {
+        arr[i, j] = N + 1; N++; j++;
+    }
+
+    i = 2;
+    j = column - 2;
+    while (i <= row - 2)
+    {
+        arr[i, j] = N + 1; N++; i++;
+    }
+
+    i = row - 2;
+    j = column - 3;
+    while (j >= 1)
+    {
+        arr[i, j] = N + 1; N++; j--;
+    }
+
+    return arr;
 }
 
 void PrintMatrix(int[,] matrix)
@@ -48,12 +104,6 @@ void PrintMatrix(int[,] matrix)
         }
         System.Console.WriteLine();
     }
-}
-
-
-void PrintAverageMatrix(double[] matrix)
-{
-    System.Console.WriteLine("Average of each column: " + string.Join("; ", matrix));
 }
 
 Main();
